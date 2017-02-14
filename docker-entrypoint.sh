a76e89a7171e7c90a6f7d823dfb4e59ad7826c0c
GMCS\sshuyskiy <sshuyskiy@gmcs.ru>
2017-02-14 16:41:06 +0300

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,7 @@ if [ ! -d "/etc/puppetlabs/puppetdb/ssl" ]; then
   /opt/puppetlabs/bin/puppet config set certname ${HOSTNAME}
   /opt/puppetlabs/bin/puppet agent --verbose --onetime --no-daemonize --waitforcert 120
   /opt/puppetlabs/server/bin/puppetdb ssl-setup -f
+  /bin/chmod 0644 /etc/puppetlabs/puppetdb/conf.d/jetty.ini 
 fi
 
 exec /opt/puppetlabs/server/bin/puppetdb "$@"
